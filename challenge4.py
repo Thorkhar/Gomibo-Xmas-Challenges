1825a7d12f007ed8b60442efaaba31f2cd934c5b
Thorkhar <100024582+Thorkhar@users.noreply.github.com>
2022-12-07 18:35:18 +0100

    almost done with ex 4

--- challenge4.py
+++ challenge4.py
@@ -1,51 +1,101 @@
 # Chocolate milk
 
 class ChocolateMilk:
-    def __init__(self, value:int) -> None:
-        self.value = value
-        self.history = [self.value]
+    def __init__(self) -> None:
+        self.history = {}
         self.snd_history = []
         with open('input4.txt', 'r') as file:
             self.instructions = file.read().split('\n')
         self.current_instruction = 0 # Index of current instruction
         self.registers = {}
+        self.create_registers()
 
     def create_registers(self):
         for command_line in self.instructions:
             cmd = command_line.split()
-            if cmd[1] not in self.registers:
+            if cmd[1] not in self.registers and cmd[1] != '1':
                 self.registers[cmd[1]] = 0
+                self.history[cmd[1]] = [0]
     
-    def choose_method_for_instr(self, instr_idx: int):
-        instruction_line = self.instructions[instr_idx].split()
-        print(instruction_line)
-        cmd = instruction_line[0]
+    def find_freq(self):
+        while self.current_instruction in range(len(self.instructions)):
+            self.ecex_instr(self.current_instruction)
+            self.current_instruction += 1
 
-    def santa_snd(self):
-        print(self.value)
+    def ecex_instr(self, instr_idx: int):
+        instr = self.instructions[instr_idx].split()
+        print(instr)
+        cmd = instr[0]
+        if cmd == 'snd':
+            self.santa_snd(instr[1])
+        elif cmd == 'set':
+            if instr[2] in self.registers:
+                y = self.registers[instr[2]]
+            else:
+                y = int(instr[2])
+            self.santa_set(instr[1], y)
+        elif cmd == 'add':
+            if instr[2] in self.registers:
+                y = self.registers[instr[2]]
+            else:
+                y = int(instr[2])
+            self.santa_add(instr[1], y)
+        elif cmd == 'mul':
+            if instr[2] in self.registers:
+                y = self.registers[instr[2]]
+            else:
+                y = int(instr[2])
+            self.santa_mul(instr[1], y)
+        elif cmd == 'mod':
+            if instr[2] in self.registers:
+                y = self.registers[instr[2]]
+            else:
+                y = int(instr[2])
+            self.santa_mod(instr[1], y)
+        elif cmd == 'rcv':
+            self.santa_rcv(instr[1])
+        elif cmd == 'jgz':
+            if instr[1] == '1':
+                x = 1
+            else:
+                x = instr[1]
 
-    def santa_set(self, y:int):
-        self.value = y
-        self.history.append(y)
+            if instr[2] in self.registers:
+                y = self.registers[instr[2]]
+            else:
+                y = int(instr[2])
+            self.santa_jgz(x, y)
 
-    def santa_add(self, y:int):
-        self.santa_set(self.value + y)
+    def santa_snd(self, x):
+        print(self.registers[x])
+        self.snd_history.append([x, self.registers[x]])
 
-    def santa_mul(self, y:int):
-        self.santa_set(self.value * y)
+    def santa_set(self, x: str, y:int):
+        if x in self.registers:
+            self.registers[x] = y
+            self.history[x].append(y)
 
-    def santa_mod(self, y:int):
-        self.santa_set(self.value % y)
+    def santa_add(self, x: str, y:int):
+        self.santa_set(x, self.registers[x] + y)
+
+    def santa_mul(self, x: str, y:int):
+        self.santa_set(x, self.registers[x] * y)
+
+    def santa_mod(self, x, y:int):
+        self.santa_set(x, self.registers[x] % y)
     
-    def santa_rcv(self):
-        if self.value != 0:
+    def santa_rcv(self, x:str):
+        if self.registers[x] != 0:
             idx = len(self.snd_history) - 1
-            self.santa_set(self.snd_history[idx])
+            self.santa_set(x, self.snd_history[idx][1])
     
-    def santa_jgz(self, y:int):
-        if self.value > 0:
-            pass
+    def santa_jgz(self, x:str, y:int):
+        if type(x) == int:
+            x_prime = x
+        else:
+            x_prime = self.registers[x]
+        if x_prime > 0:
+            self.current_instruction -= (y + 1)
 
-x = ChocolateMilk(0)
-x.create_registers()
-print(x.registers)
+x = ChocolateMilk()
+x.find_freq()
