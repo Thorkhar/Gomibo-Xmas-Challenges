af84fdf85cb9dbe38982c38ab3cefb2cf61a3350
Thorkhar <100024582+Thorkhar@users.noreply.github.com>
2022-12-07 11:24:19 +0100

    solved challenge 1

---
+++ challenge1.py
@@ -0,0 +1,18 @@
+def parse(instr: str) -> int:
+    floor = 0
+    open_cnt = 0
+    close_cnt = 0
+    instr_array = list(instr)
+    for i in instr_array:
+        if i == "(":
+            floor += 1
+            open_cnt += 1
+        if i == ")":
+            floor -= 1
+            close_cnt += 1
+    return [floor, open_cnt, close_cnt]
+
+with open('input1.txt', 'r') as file:
+    instructions = file.read()
+
+print(parse(instructions))
